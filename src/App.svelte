fdeb98b867bcd10eb0388b69210e06bbfda5aefb
Andy-Castle <masterchief_and@live.com>
2025-07-31 21:17:25 -0500

    Nested Components, aqui se vio como podemos importar otros componentes a otro y que aunque uno tenga el mismo elemento html, el estilo no le afecta al otro y que los componentes deben de ir capitalzados para distinguirlos de los elementos de HTML

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
   import YourFirstComponent from "./Basic Svelte/Introduction/YourFirstComponent.svelte";
   import DynamicAttributes from "./Basic Svelte/Introduction/DynamicAttributes.svelte";
   import Styling from "./Basic Svelte/Introduction/Styling.svelte";
+  import Main from "./Basic Svelte/Introduction/NestedComponents/Main.svelte";
 
 </script>
 
 <main>
-<Styling/>
+<Main/>
 </main>
 
