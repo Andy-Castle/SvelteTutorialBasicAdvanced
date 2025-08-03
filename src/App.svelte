85f8cb3642c4cb4e808948fceea54ee8b962079b
Andy-Castle <masterchief_and@live.com>
2025-08-03 18:49:36 -0500

    State, Aqui se vio como utilizar un sistema de reactividad, para mantener DOM sincronizado con el estado de la aplicación, como en este caso la respuesta a un evento, aqui en este ejemplo se usan las runas de svelte, que se usa para tener un estado

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,12 @@
   // import DynamicAttributes from "./Basic Svelte/Introduction/DynamicAttributes.svelte";
   // import Styling from "./Basic Svelte/Introduction/Styling.svelte";
   // import Main from "./Basic Svelte/Introduction/NestedComponents/Main.svelte";
-  import HtmLtags from "./Basic Svelte/Introduction/HTMLtags.svelte";
+  // import HtmLtags from "./Basic Svelte/Introduction/HTMLtags.svelte";
+  import State from "./Basic Svelte/Reactivity/State.svelte";
 
 </script>
 
 <main>
-<HtmLtags/>
+<State/>
 </main>
 
---
+++ src/Basic Svelte/Reactivity/State.svelte
@@ -0,0 +1,17 @@
+<!-- En svelte tiene un sistema de reactividad para mantener
+ el DOM sincronizado con el estado de la aplicacion,
+ como puede ser la respueata a un evento -->
+<script>
+	let count = $state(0);
+  /*Esto se le llama runa, asi se le llama en Svelte*/
+
+	function increment() {
+		// TODO implement
+    count += 1
+	}
+</script>
+
+<button onclick={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
