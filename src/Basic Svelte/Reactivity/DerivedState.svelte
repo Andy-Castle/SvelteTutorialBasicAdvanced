fed1d0a9c7cd252e93c3116a9b375010f3948c78
Andy-Castle <masterchief_and@live.com>
2025-08-03 19:16:36 -0500

    Derived State, en este ejemplo use en varios casos el $derived, esta runa sera util cuando mediante una api obtenga datos y quiero que estas se actualicen automaticamente

--- src/Basic Svelte/Reactivity/DerivedState.svelte
+++ src/Basic Svelte/Reactivity/DerivedState.svelte
@@ -4,6 +4,9 @@
 <script>
 	let numbers = $state([1, 2, 3, 4]);
   let total = $derived(numbers.reduce((t, n) => t + n, 0));
+  let totalMult = $derived(numbers.reduce((t, n) => t * n))
+  let totalDiv = $derived(numbers.reduce((t, n) => t / n))
+  let totalMin = $derived(numbers.reduce((t, n) => t - n))
 
 	function addNumber() {
 		numbers.push(numbers.length + 1);
@@ -11,6 +14,9 @@
 </script>
 
 <p>{numbers.join(' + ')} = {total}</p>
+<p>{numbers.join(' * ')} = {totalMult}</p>
+<p>{numbers.join(' / ')} = {totalDiv}</p>
+<p>{numbers.join(' - ')} = {totalMin}</p>
 
 <button onclick={addNumber}>
 	Add a number
