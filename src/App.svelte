fcfa55344c25b4e511830b7472286858af421617
Andy-Castle <masterchief_and@live.com>
2025-08-03 19:09:34 -0500

    Derived State, a veces se necesita derivar un estado de otro estado, asi que se usa la runa de $derived, esta sirve para reevaluar siempre que sus dependencias se actualizan

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,11 @@
   // import Main from "./Basic Svelte/Introduction/NestedComponents/Main.svelte";
   // import HtmLtags from "./Basic Svelte/Introduction/HTMLtags.svelte";
   // import State from "./Basic Svelte/Reactivity/State.svelte";
-  import DeepState from "./Basic Svelte/Reactivity/DeepState.svelte";
+  // import DeepState from "./Basic Svelte/Reactivity/DeepState.svelte";
+  import DerivedState from "./Basic Svelte/Reactivity/DerivedState.svelte";
 </script>
 
 <main>
-<DeepState/>
+<DerivedState/>
 </main>
 
---
+++ src/Basic Svelte/Reactivity/DerivedState.svelte
@@ -0,0 +1,20 @@
+<!-- En ocasiones se necesita derivar un estado de otro estado
+ asi que se usa la runa de $derived -->
+
+<script>
+	let numbers = $state([1, 2, 3, 4]);
+  let total = $derived(numbers.reduce((t, n) => t + n, 0));
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+	}
+</script>
+
+<p>{numbers.join(' + ')} = {total}</p>
+
+<button onclick={addNumber}>
+	Add a number
+</button>
+
+<!-- Aqui la expresion $derived, la declaración sera reevaluada siempre
+ que sus dependencias, en este caso el numbers se actualizan -->
