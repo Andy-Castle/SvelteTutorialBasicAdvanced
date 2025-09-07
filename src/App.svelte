caa7ee03edf5f952704297e4c112e5713da37238
Andy-Castle <masterchief_and@live.com>
2025-09-06 20:54:23 -0500

    Checkbox Inputs, aqui se vio como podemos usar el bind: para los inputs de tipo checked

--- src/App.svelte
+++ src/App.svelte
@@ -25,11 +25,11 @@
   // import ComponentEvents from "./Basic Svelte/Events/ComponentEvents/ComponentEvents.svelte";
   // import SpreadingEvents from "./Basic Svelte/Events/SpreadingEvents/SpreadingEvents.svelte";
   // import TextInputs from "./Basic Svelte/Bindings/TextInputs.svelte"
-    import NumericInputs from "./Basic Svelte/Bindings/NumericInputs.svelte";
-
+  // import NumericInputs from "./Basic Svelte/Bindings/NumericInputs.svelte";
+  import CheckboxInputs from "./Basic Svelte/Bindings/CheckboxInputs.svelte";
 </script>
 
 <main>
-<NumericInputs/>
+<CheckboxInputs/>
 </main>
 
---
+++ src/Basic Svelte/Bindings/CheckboxInputs.svelte
@@ -0,0 +1,25 @@
+<script>
+	let yes = $state(false);
+</script>
+
+<!-- Los checbox tambien se utilizan para alternar entre estados -->
+ <!-- En lugar de vincularse a input.value, nos vinculamos a input.checled -->
+
+<label>
+	<input type="checkbox" bind:checked={yes} />
+	Yes! Send me regular email spam
+</label>
+
+{#if yes}
+	<p>
+		Thank you. We will bombard your inbox and sell
+		your personal details.
+	</p>
+{:else}
+	<p>
+		You must opt in to continue. If you're not
+		paying, you're the product.
+	</p>
+{/if}
+
+<button disabled={!yes}>Subscribe</button>
